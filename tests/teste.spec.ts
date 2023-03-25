a4d977898e0be4487dc09759c112ca2f2c453da6
luizpbello <luizpaulopinto25@gmail.com>
2023-03-25 12:21:21 -0300

    Testando Husky

---
+++ tests/teste.spec.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+
+describe("Teste inicial genérico", () => {
+  const stringTeste = ["Teste1", "Teste2"];
+
+  it('verifica se o primeiro elemento é "Teste1"', () => {
+    expect(stringTeste[0]).toBe("Teste1");
+  });
+
+  it('verifica se o primeiro elemento é "Teste2"', () => {
+    expect(stringTeste[1]).toBe("Teste2");
+  });
+});
