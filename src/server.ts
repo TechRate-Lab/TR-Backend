ed1e2cbd6127c0fb4f1a0da4d61047e41935ceb6
luizpbello <luizpaulopinto25@gmail.com>
2023-03-25 21:55:04 -0300

    refactor: new server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import { port } from "./config/vars";
 
 
 app.listen(3000, () => {
-  console.log(`\u{1F680} Servidor rodando na porta ${port}`);
+  console.log(`\u{1F680} Servidor rodando  na porta ${port}`);
 });
