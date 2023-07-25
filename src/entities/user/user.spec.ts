ca10b4e710e02d0e590094503fd0d4d849ba4820
Luiz Bello <88148849+luizpbello@users.noreply.github.com>
2023-07-25 12:22:52 -0300

    feat: adicionando usecase, controller e dto (#43)

--- src/entities/user/user.spec.ts
+++ src/entities/user/user.spec.ts
@@ -1,19 +1,17 @@
-import { expect, test } from "vitest";
+import { expect, test, it } from "vitest";
 import { User } from "./user";
 
-
-
 test("criar usuário", () => {
-  const user = new User({
-    name: "Fulano",
-    email: "fulanosilva@gmail.com",
-    password: "123",
-    confirmPassword: "123",
-  });
+  it("Deve ser possível criar um usuário"),
+    async () => {
+      const user = new User({
+        name: "Fulano",
+        email: "fulanosilva@gmail.com",
+        password: "123",
+      });
+      expect(user).toBeInstanceOf(User);
 
-  expect(user).toBeInstanceOf(User);
-
-  expect(user.name).toBe("Fulano");
-  expect(user.email).toBe("fulanosilva@gmail.com");
-  expect(user.password).toEqual(user.confirmPassword);
+      expect(user.name).toBe("Fulano");
+      expect(user.email).toBe("fulanosilva@gmail.com");
+    };
 });
